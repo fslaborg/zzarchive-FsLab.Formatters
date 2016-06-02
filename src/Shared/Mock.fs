cb67c8317b33204762964742828f3efc2cc4a2a5
Tomas Petricek <tomas@tomasp.net>
2016-06-02 22:12:53 +0200

    Add text-based printers

--- src/Shared/Mock.fs
+++ src/Shared/Mock.fs
@@ -9,6 +9,6 @@ module MockFsiObject =
   type FsiObject() =
     member x.HtmlPrinterParameters : System.Collections.Generic.IDictionary<string, obj> = failwith "Mock"
     member x.AddHtmlPrinter<'T>(f:'T -> seq<string * string> * string) : unit = failwith "Mock"
-    
+    member x.AddPrinter<'T>(f:'T -> string) : unit = failwith "Mock"  
   let fsi = FsiObject()
-
+  
